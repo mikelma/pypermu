ee627b01c17bd0e3f0cd13ae0310edbd7f561278
mikelma <mikelma7@gmail.com>
2021-06-18 10:06:59 +0200

    docs updated

--- src/distributions/placket_luce.rs
+++ src/distributions/placket_luce.rs
@@ -13,7 +13,7 @@ pub fn init_mod_pl(py: Python) -> PyResult<&PyModule> {
 }
 
 /// Samples a given number of samples from a weights vector of a Placket-Luce distribution.
-/// **Note:** The weights vector does not need to be normalized.
+/// **Note:** The weights vector does not need to be normalized, however, weigths must be positive.
 #[pyfunction]
 pub fn sample_pl(weights: Vec<f32>, n_samples: usize) -> PyResult<Population> {
     let n = weights.len();
